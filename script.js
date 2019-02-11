1ff831033d72a1a97fdbc350a819299558278f64
eyankina <ekaterina.yankina@mail.ru>
2019-02-11 17:22:37 +0300

    getJSON function added

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 'use strict';
 $(function () {
-    // Ваш код
+    //получить аяксом JSON
+    let getData = $.getJSON('products.json', function(data){
+
+    })
+        .success(function(data) {
+            alert("Успешное выполнение");
+            let items = [];
+            $.each(data, function(key, val){
+                items.push('<li id="' + key + '">' + val + '</li>');
+            });
+
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('body');
+        })
+        .error(function() { alert("Ошибка выполнения"); })
+        .complete(function() { alert("Завершение выполнения"); });
 });
