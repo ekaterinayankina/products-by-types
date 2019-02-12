981dc467964201c7960399d4a14c7780b9523345
eyankina <ekaterina.yankina@mail.ru>
2019-02-12 15:48:54 +0300

    add rendering result

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 'use strict';
+
 $(function () {
-    //получить аяксом JSON
-    $.getJSON('products.json', function(data) {
-        let sale = data.filter(function(product) {
-            return product.type==='sale';
-        });
-        let promo = data.filter(function(product) {
-            return product.type==='promo';
-        });
-        let recommended = data.filter(function(product) {
-            return product.type==='recommended';
-        });
-        let sortDataSet = [
-                {title:'Распродажа', item: sale},
-                {title:'Промо акция', item: promo},
-                {title:'Рекомендуемые товары', item: recommended}
-        ];
+    $.getJSON('products.json', function (data) {
+
+        let sale = {
+            title: "Распродажа",
+            item: filterData(data, 'sale')
+        };
+
+        let promo = {
+            title: "Промо-акция",
+            item: filterData(data, 'promo')
+        };
+
+        let recommended = {
+            title: "Рекомендуемые товары",
+            item:  filterData(data, 'recommended')
+        };
+
+        let tmpl = document.getElementById('productTemplate').innerHTML.trim();
+        tmpl = _.template(tmpl);
+
+
+        renderTemplate(sale, tmpl, "sale");
+        renderTemplate(promo, tmpl, "promo");
+        renderTemplate(recommended, tmpl, "recommended");
     });
-    var tmpl = _.template(document.getElementById('productTemplate').innerHTML);
-    var result = tmpl({sortDataSet});
-    document.write( result );
 });
+
+function filterData(data, text) {
+    return data.filter(function (product) {
+        return product.type === text
+    });
+}
+
+function renderTemplate(list, tmpl, id) {
+    document.getElementById(id).innerHTML = tmpl({
+        temp: list
+    });
+}
