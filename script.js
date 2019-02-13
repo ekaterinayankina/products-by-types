e28191664b68ce31d0fefba106fb4b7ff5caeb84
eyankina <ekaterina.yankina@mail.ru>
2019-02-13 10:56:02 +0300

    add timeout before data loading to show fancybox working

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 'use strict';
 $(function () {
     $.fancybox.showLoading();
-    $.getJSON('products.json', function (data) {
-        let sale = {
-            title: "Распродажа",
-            item: filterData(data, 'sale')
-        };
+    setTimeout(function () {
+        $.getJSON('products.json', function (data) {
+            let sale = {
+                title: "Распродажа",
+                item: filterData(data, 'sale')
+            };
 
-        let promo = {
-            title: "Промо-акция",
-            item: filterData(data, 'promo')
-        };
+            let promo = {
+                title: "Промо-акция",
+                item: filterData(data, 'promo')
+            };
 
-        let recommended = {
-            title: "Рекомендуемые товары",
-            item:  filterData(data, 'recommended')
-        };
+            let recommended = {
+                title: "Рекомендуемые товары",
+                item: filterData(data, 'recommended')
+            };
 
-        let tmpl = document.getElementById('productTemplate').innerHTML.trim();
-        tmpl = _.template(tmpl);
+            let tmpl = document.getElementById('productTemplate').innerHTML.trim();
+            tmpl = _.template(tmpl);
 
-        renderTemplate(sale, tmpl, "sale");
-        renderTemplate(promo, tmpl, "promo");
-        renderTemplate(recommended, tmpl, "recommended");
-    })
-        .done(function () {
-            console.log("Success");
+            renderTemplate(sale, tmpl, "sale");
+            renderTemplate(promo, tmpl, "promo");
+            renderTemplate(recommended, tmpl, "recommended");
         })
-        .fail(function () {
-            console.log("Try again");
-        })
-        .always(function () {
-            $.fancybox.hideLoading();
-            console.log("Complete");
-        });
+            .done(function () {
+                console.log("Success");
+            })
+            .fail(function () {
+                console.log("Try again");
+            })
+            .always(function () {
+                $.fancybox.hideLoading();
+                console.log("Complete");
+            });
+    }, 3000);
 });
 
 function filterData(data, text) {
