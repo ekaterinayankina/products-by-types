e83e9d727ec1a78dece21dc308e98018a996dd45
eyankina <eyankina@oggettoweb.com>
2019-04-11 19:26:26 -0700

    Refactor code
    -Add method showResult
    
    [#re master]

--- script.js
+++ script.js
@@ -2,30 +2,9 @@
 $(function () {
     $.fancybox.showLoading();
     setTimeout(function () {
-        $.getJSON('products.json', function (data) {
-            let sale = {
-                title: "Распродажа",
-                item: filterData(data, 'sale')
-            };
-
-            let promo = {
-                title: "Промо-акция",
-                item: filterData(data, 'promo')
-            };
-
-            let recommended = {
-                title: "Рекомендуемые товары",
-                item: filterData(data, 'recommended')
-            };
-
-            let tmpl = document.getElementById('productTemplate').innerHTML.trim();
-            tmpl = _.template(tmpl);
-
-            renderTemplate(sale, tmpl, "sale");
-            renderTemplate(promo, tmpl, "promo");
-            renderTemplate(recommended, tmpl, "recommended");
-        })
-            .done(function () {
+        $.getJSON('products.json')
+            .done(function (data) {
+                showResult(data);
                 console.log("Success");
             })
             .fail(function () {
@@ -35,7 +14,7 @@ $(function () {
                 $.fancybox.hideLoading();
                 console.log("Complete");
             });
-    }, 3000);
+    }, 2000);
 });
 
 function filterData(data, text) {
@@ -48,4 +27,28 @@ function renderTemplate(list, tmpl, id) {
     document.getElementById(id).innerHTML = tmpl({
         temp: list
     });
+}
+
+function showResult(products) {
+    let sale = {
+        title: "Распродажа",
+        item: filterData(products, 'sale')
+    };
+
+    let promo = {
+        title: "Промо-акция",
+        item: filterData(products, 'promo')
+    };
+
+    let recommended = {
+        title: "Рекомендуемые товары",
+        item: filterData(products, 'recommended')
+    };
+
+    let tmpl = document.getElementById('productTemplate').innerHTML.trim();
+    tmpl = _.template(tmpl);
+
+    renderTemplate(sale, tmpl, "sale");
+    renderTemplate(promo, tmpl, "promo");
+    renderTemplate(recommended, tmpl, "recommended");
 }
